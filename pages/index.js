17378aa5d22588f129afa3da656e86509cf53823
AYYBEEYOU <ayybeeyou@gmail.com>
2023-11-09 11:37:28 +0100

    initial startup code

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            This project belongs to father of asahd
+            This project belongs to Abubakar
           </p>
         </a>
 
